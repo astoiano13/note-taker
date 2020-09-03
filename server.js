0cef65ca4f4a2c72ada2a5d6a91e0185998191ba
Aaron Stoiano <aaronstoiano@gmail.com>
2020-09-02 22:58:26 -0400

    added get routes then got stuck

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 var jdb = require("db-json");
 var path = require("path");
-
+var express = require("express");
 
 var app = express();
 var PORT = process.env.PORT || 1337;
 
+// prepare express to parse data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// routes
+//=============================================================
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    return db.json(notes);
+});
+
+
+app.post(/api/notes)
+//pretty much stuck past this point....
+
+
 
 
 app.listen(PORT, function(){
